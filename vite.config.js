71a8967ef0b51efb35f078eede1dcca869895e09
samyfel <94083309+samyfel@users.noreply.github.com>
2024-12-24 21:25:21 +0100

    Updated package.json and Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
-})
+  base: '/samyportfolio/', // This should match your repository name
+});
